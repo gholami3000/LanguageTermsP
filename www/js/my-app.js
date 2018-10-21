e8eea43b2d95dc35773abf4d61752faf87a0acc1
Ho Gholami <gholami3000@gmail.com>
2018-10-21 17:00:36 +0330

    درست کردن قسمت درباره ما
    Signed-off-by: Ho Gholami <gholami3000@gmail.com>

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,158 +1,188 @@
 ﻿// Initialize app
 var myApp = new Framework7({
-    material: true,
-    // domCache: true
+  material: true
+  // domCache: true
 });
-
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+var mainView = myApp.addView(".view-main", {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
 });
 
+function MakeCall(e) {
+  document.location.href = "tel:" + "09357144541";
+}
+
 var ToastOptions = {
-    // Callback gets called when toast is hidden
-    onHide: function () {
-        console.log('hidden');
-    },
-    duration: 3000 // Hide toast after 2 seconds
+  // Callback gets called when toast is hidden
+  onHide: function() {
+    console.log("hidden");
+  },
+  duration: 3000 // Hide toast after 2 seconds
 };
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function () {
-    console.log("Device is ready!");
+$$(document).on("deviceready", function() {
+  console.log("Device is ready!");
 
-    getScope("MainAppId").getItem();
+  getScope("MainAppId").getItem();
 
+  document.addEventListener("backbutton", callbackFunction, false);
+  // alert("backbutton");
+  function callbackFunction() {
+    if (mainView.activePage.name === "index") {
+      myApp.modal({
+        title: "خروج",
+        text: "آیا می خواهید ازبرنامه  خارج شوید؟",
+        buttons: [
+          {
+            text: "خروج از برنامه",
+            onClick: function() {
+              navigator.app.exitApp();
+            }
+          },
+
+          {
+            text: "بی خیال",
+            onClick: function() {}
+          }
+        ]
+      });
+    } else if (mainView.activePage.name === "registerEdit") {
+      RefreshMyList();
+      mainView.router.back();
+      //ajaxRequest.abort();
+      myApp.hidePreloader();
+    } else {
+      mainView.router.back();
+      //ajaxRequest.abort();
+      myApp.hidePreloader();
+    }
+  }
 });
 
-
-
-var TermApp = angular.module('TermApp', []);
+var TermApp = angular.module("TermApp", []);
 //var APP = APP || {};
 
 function getScope(divId) {
-    return angular.element(document.getElementById(divId)).scope();
+  return angular.element(document.getElementById(divId)).scope();
 }
 
 function Compile(divId) {
-    getScope(divId).compileDOM("#" + divId);
+  getScope(divId).compileDOM("#" + divId);
 }
 
-TermApp.controller('MainCtrl', function ($scope, $compile, $http) {
+TermApp.controller("MainCtrl", function($scope, $compile, $http) {
+  $scope.getItem = function() {
+    myApp.showPreloader("لطفا کمی صبر کنید");
 
-    $scope.getItem = function () {
-        myApp.showPreloader("لطفا کمی صبر کنید");
+    $http({
+      method: "GET",
+      url: "http://www.gholami3000.com/Translate/Api/TermList"
+      // headers: { 'Content-Type': undefined },
+      //data:data
+    }).then(
+      function mySucces(response) {
+        $scope.myData = response.data;
+        console.log(response.data);
+        myApp.hidePreloader();
+      },
+      function myError(error) {
+        console.log(error);
+        myApp.hidePreloader();
+      }
+    );
+  };
 
-        $http({
-            method: "GET",
-            url: "http://www.gholami3000.com/Translate/Api/TermList",
-            // headers: { 'Content-Type': undefined },
-            //data:data
-        }).then(function mySucces(response) {
-            $scope.myData = response.data;
-            console.log(response.data);
-            myApp.hidePreloader();
-        }, function myError(error) {
-            console.log(error);
-            myApp.hidePreloader();
-        });
-    }
+  $scope.term = "";
+  $scope.translate = "";
+  $scope.registerItem = function() {
+    // alert();
+    // $scope.term="hi";
+    // $scope.translate="hi2";
 
-       $scope.term = "";
-       $scope.translate = "";
-       $scope.registerItem = function () {
-          // alert();
-       // $scope.term="hi"; 
-       // $scope.translate="hi2"; 
+    // var term=  angular.element('#term').val();
+    //var translate= angular.element('#translate').val();
+    //alert(term);
+    myApp.showPreloader("لطفا کمی صبر کنید");
 
-     // var term=  angular.element('#term').val();
-      //var translate= angular.element('#translate').val();
-       //alert(term);
-        myApp.showPreloader("لطفا کمی صبر کنید");
+    $http({
+      method: "post",
+      url: "http://www.gholami3000.com/Translate/Term/Create",
+      // headers: {
+      //     'Content-Type': 'application/json',
+      // },
+      // headers: { 'Content-Type': application/json },
+      data: { term: $scope.term, translate: $scope.translate }
+    }).then(
+      function mySucces(response) {
+        if (response.data.Success) {
+          var toast = myApp.toast(response.data.Message, "", ToastOptions);
+          toast.show();
+          $scope.term = "";
+          $scope.translate = "";
+        } else {
+          var toast = myApp.toast("لطفا دوباره سعی کنید", "خطا", ToastOptions);
+          toast.show();
+        }
+        myApp.hidePreloader();
+      },
+      function myError(error) {
+        console.log(error);
+        myApp.hidePreloader();
+      }
+    );
+  };
 
-        $http({
-            method: "post",
-            url: "http://www.gholami3000.com/Translate/Term/Create",
-            // headers: {
-            //     'Content-Type': 'application/json',
-            // },
-            // headers: { 'Content-Type': application/json },
-             data:{term:$scope.term,translate:$scope.translate}
-        }).then(function mySucces(response) {
-            if(response.data.Success){
-                var toast = myApp.toast(response.data.Message, '', ToastOptions);
-                toast.show();
-                $scope.term = "";
-                $scope.translate = "";
-            }
-            else{
-                var toast = myApp.toast('لطفا دوباره سعی کنید', 'خطا', ToastOptions);
-                toast.show();
-            }
-            myApp.hidePreloader();
-        }, function myError(error) {
-            console.log(error);
-            myApp.hidePreloader();
-        });
-    }
-
-    $scope.compileDOM = function ($el) {
-        ($compile($el))($scope);
-        $scope.$apply();
-    };
-    //$scope.compile = function () {
-    //    //  $scope.items.push({ name: 'item ' + (new Date().getTime()) });
-    //    $scope.compileDOM("#dest");
-    //};
+  $scope.compileDOM = function($el) {
+    $compile($el)($scope);
+    $scope.$apply();
+  };
+  //$scope.compile = function () {
+  //    //  $scope.items.push({ name: 'item ' + (new Date().getTime()) });
+  //    $scope.compileDOM("#dest");
+  //};
 });
 
-
-
-
 function LoadNewContent(divId) {
-    $("#dest").html('<p>Name : <input type="text" ng-model="family"></p><h1>Hello {{family}}</h1>');
-    getScope(divId).compileDOM("#" + divId);
+  $("#dest").html(
+    '<p>Name : <input type="text" ng-model="family"></p><h1>Hello {{family}}</h1>'
+  );
+  getScope(divId).compileDOM("#" + divId);
 
-    // angular.element('#dest').scope().compileDOM("#dest");
-
-}//
+  // angular.element('#dest').scope().compileDOM("#dest");
+} //
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('registerTerm', function (page) {
-    // Do something here for "about" page
-    // getScope("TermAppContainer").apply();
-})
+myApp.onPageInit("registerTerm", function(page) {
+  // Do something here for "about" page
+  // getScope("TermAppContainer").apply();
+});
 
 // Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
+$$(document).on("pageInit", function(e) {
+  // $scope.digest();
 
+  // Get page data from event data
+  var page = e.detail.page;
 
-    // $scope.digest();
-
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'registerTerm') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        // myApp.alert('Here comes About page');
-        //$("#compile").trigger("click");
-    }
-})
+  if (page.name === "registerTerm") {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    // myApp.alert('Here comes About page');
+    //$("#compile").trigger("click");
+  }
+});
 
 // Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="registerTerm"]', function (e) {
-    //کامپایل دوباره DOM
-    //کامپایل بر اساس id
-    Compile('frmRegister');
-})
-
-
-
+$$(document).on("pageInit", '.page[data-page="registerTerm"]', function(e) {
+  //کامپایل دوباره DOM
+  //کامپایل بر اساس id
+  Compile("frmRegister");
+});
 
 //$("#registerTerm").on("click", function () {
 //       mainView.router.loadPage('registerTerm.html');
@@ -173,9 +203,6 @@ $$(document).on('pageInit', '.page[data-page="registerTerm"]', function (e) {
 //        }
 //    };
 //});
-
-
-
 
 //function MainController($scope, $http) {
 ////$scope.getItem = function () {
@@ -206,9 +233,7 @@ $$(document).on('pageInit', '.page[data-page="registerTerm"]', function (e) {
 
 //    });
 
-
 //}
-
 
 //termApp.controller('MainController', function ($scope, $http) {
 //    $scope.getItem = function () {
@@ -229,8 +254,4 @@ $$(document).on('pageInit', '.page[data-page="registerTerm"]', function (e) {
 //        });
 //    }
 
-
-
-
 //});
-
